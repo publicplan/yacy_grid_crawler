b523e3f0a45d00f512f18766b8e32c5a043e7df6
Yannic Labonte <labonte@publicplan.de>
2019-08-08 15:32:57 +0200

    Update Dockerfile
    Update gradle version 4.10 -> 5.4
    Update shadowJar plugin 2.0.4 -> 5.0.0
    Use gradle deafult build distribution zip instead of shadow jar

--- docker/crawler_vsm_blacklist.sh
+++ docker/crawler_vsm_blacklist.sh
@@ -4,12 +4,10 @@ for format in year vJ cjahr yearID month date \\/; do
   currentYear=$(date +'%Y')
   for sub in 4 3 -3 -4; do
     year=`expr $currentYear - $sub`
-    if [ $format == "\\/" ]; then
+    if [ "$format" = "\\/" ]; then
       echo ".*\\/$year\\/.*" >> "$1"
     else
       echo ".*$format=$year.*" >> "$1"
     fi
   done
 done
-
-
